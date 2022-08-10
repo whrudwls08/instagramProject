ae2582c12f3f0f5bcfa637a756952be76dad9490
DESKTOP-S5M2HJU\Jo <whrudwls08@naver.com>
2022-08-10 10:11:16 +0900

    이미지 추가 및 store 영역 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [option] = useState(['home','dm','new','star','heart','myPage'])
+  let [users] = useState(['조경진','김종국','유재석','강호동'])
   return (
     <div className="App">
       
+      <div className="navMenu">
+        <div className="navTitle">
+          Jostagram
+        </div>
+        <div className="navInput">
+          <input placeholder="검색" id="search"/>
+        </div>
+        <div className="navOption">
+          {
+            option.map((val, index)=>{
+              return(
+                <NavOption val={val} index={index}/>
+              )
+            })   
+          }
+        </div>
+      </div>
+        <div className="section">
+          <div className='usersPost'>
+            <div className='userStore'> 
+              <div className='userStoreList'>
+              {
+                users.map((val, index)=>{
+                  return(
+                    <UserStoreList val={val} index={index}/>
+                  )
+                })
+              }
+              </div>
+            </div>
+            <div className='userPost'>
+            </div>
+          </div>
+          <div className="newFriend">
+
+          </div>
+        </div>
     </div>
   );
 }
+function NavOption(props){
+  return(
+    <div className={'navOption' + props.val +' subOption'} >
+      <img src={'./img/'+props.val+'.png'} alt="optionList" onClick={()=>{
+      }}></img>
+    </div> 
+  )
+}
 
+function UserStoreList(props){
+  return(
+    <div className='userStoreListuserList'>
+      <div className={'userStoreListuserList'+props.index+' userImage'}>
+      </div>
+      <div className='userStoreListName'>
+        <span>{props.val}</span>
+      </div>
+    </div>
+  )
+}
 export default App;
